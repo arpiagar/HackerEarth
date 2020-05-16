b38b0fc5666d7b6c03536f7aaf4171d587996eb7
Arpit Agarwal <arpit.agarwal@booking.com>
2020-05-17 01:35:52 +0200

    Adding more coding problems

--- minimum_path_sum/solution.py
+++ minimum_path_sum/solution.py
@@ -10,5 +10,5 @@ class Solution:
         for i in range(1, m):
             for j in range(1, n):
                 grid[i][j] += min(grid[i-1][j], grid[i][j-1])
-        return grid[m-1][n-1]i
+        return grid[m-1][n-1]
 
